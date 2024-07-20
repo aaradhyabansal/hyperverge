b3a7f9213fc43d6335e0423f440d96ecd69b263c
Turbobot <turbobot@vercel.com>
2024-07-20 16:59:19 +0530

    feat(create-turbo): apply official-starter transform

--- 100xdevs/week-6/week-6-experiments/src/App.jsx
+++ 100xdevs/week-6/week-6-experiments/src/App.jsx
@@ -1,43 +1,33 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState, useMemo, useCallback } from "react";
 
 function App() {
-  const [id, setId] = useState(1);
-  let arr = [1, 2, 3, 4];
-  return (
-    <div>
-      {arr.map((val) => {
-        return (
-          <button
-            key={val}
-            className="h-300 w-300"
-            onClick={() => {
-              setId(val);
-              console.log(id);
-            }}
-          >
-            {val}
-          </button>
-        );
-      })}
-      <TODO id={id} />
-    </div>
-  );
-}
-function TODO({ id }) {
-  const [todos, setTodos] = useState({});
-  useEffect(() => {
-    axios
-      .get("https://sum-server.100xdevs.com/todo?id=" + id)
-      .then((response) => {
-        setTodos(response.data.todo);
-      });
+  const [count, setCount] = useState(0);
+  const inputFunction = useCallback(() => {
+    console.log("inputFunction");
   }, []);
   return (
     <div>
-      <h1>{todos.title}</h1>
-      <h4>{todos.description}</h4>
+      <Buttoncomponent inputFunction={inputFunction} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Counter({count})
+      </button>
     </div>
   );
 }
+
+const Buttoncomponent = memo(({ inputFunction }) => {
+  console.log("Child Render");
+
+  return (
+    <div>
+      <button>Button Clicked</button>
+    </div>
+  );
+});
+
 export default App;
